8f08b7f2d76b242b309e405da22bb4d8657b2e87
lT1anRu161 <ltianrui61@gmail.com>
2022-04-29 17:55:11 +0800

    Update deploy  workflows.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*
  * @Author: ExpLife0011
  * @Date: 2022-04-29 14:58:58
- * @LastEditTime: 2022-04-29 15:04:42
+ * @LastEditTime: 2022-04-29 17:50:12
  * @LastEditors: ExpLife0011
  * @Description:
  * @FilePath: /full-stack-open-pokedex/app.js
@@ -14,6 +14,14 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send(1)
+})
 
 app.listen(PORT, () => {
   console.log('server started on port 5000')
